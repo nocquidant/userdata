1219fc5e1db737d37e6217e1b4d79c485bd96fe4
Nicolas Ocquidant <nocquidant@gmail.com>
2020-01-23 10:28:27 +0100

    Update setupApp.sh

--- setupApp.sh
+++ setupApp.sh
@@ -1,4 +1,8 @@
 echo "Setting up a simple HTTP server" 
-mkdir /var/www/ && cd /var/www/ 
+mkdir -p /var/www/ 
+usermod -a -G app ec2-user
+chown -R ec2-user:app /var/www
+chmod 2775 /var/www
+cd /var/www/ 
 curl -o- https://raw.githubusercontent.com/nocquidant/userdata/master/index.js
 node index.js
